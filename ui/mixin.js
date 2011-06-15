08dbc3c23d615529e66ceac3b368fc3e84d93afa
David Jones <davidjones@gamestop.com>
2011-06-15 16:31:27 -0500

    Small fix for auto horiz alignment when appending * Also remove unused function

--- ui/mixin.js
+++ ui/mixin.js
@@ -6,19 +6,10 @@
         view.append = function(obj) {
             var objTotalHeight = obj.height + (obj.top ? obj.top : 0);
             obj.top = (obj.top ? obj.top : 0) + yOffset;
-            obj.left = (obj.left ? obj.left : 0);
+			obj.left = obj.left || (obj.right ? undefined : 0);
             view.add(obj);
             yOffset += objTotalHeight;
         };
-
-		view.appendOnCreate = function(createFunction, createParams) {
-			var obj = createFunction(createParams.merge({
-	            top:(createParams.top || 0) + yOffset,
-	            left:(createParams.left || 0)
-			}));
-            view.add(obj);
-            yOffset +=  obj.height + (createParams.top || 0);
-		};
 
 		view.currentY = function() {
 			return yOffset;
